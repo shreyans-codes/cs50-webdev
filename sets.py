e6187b2f121e6d7d6f90340acf99b9ff57ed75ed
Shreyans Sethia <sheru.sherki28@gmail.com>
2020-06-06 12:24:04 +0530

    started learning django

---
+++ sets.py
@@ -0,0 +1,14 @@
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+s.remove(3)
+print(s)
+
+houses = {"Harry":"Gryffindor", "Draco":"Slytherin"}
+
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Hermione"])
